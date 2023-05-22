b2c4539d91e03c826856d2fe25cbec7a95c5ab1d
Alex Oliveira <alexoalves10@gmail.com>
2023-05-22 14:27:06 -0300

    Exercicios Python

---
+++ Exercicios_curso_python.py
@@ -0,0 +1,11 @@
+#exercicios seçao 05-condicionais
+
+#exer1:
+print("digite dois numeros, o maior deles sera retornado:")
+first_number = int(input())
+second_number = int(input())
+
+if first_number > second_number:
+    print(f'O maior numero digitado foi:{first_number}')
+else:
+    print(f'O maior numero digitado foi:{second_number}')
